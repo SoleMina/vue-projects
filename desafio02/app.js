b7372918f248dcc812abe6366a7b021b3403f3f1
SoleMina <karina.pradogutierrez@gmail.com>
2022-09-05 12:10:53 -0500

    Feat: update challenge

--- desafio02/app.js
+++ desafio02/app.js
@@ -13,57 +13,58 @@ var app = new Vue({
   el: "#app",
   data: {
     title: "Desafío 2",
-    codermeals: [
+    coderProducts: [
       {
         id: 1,
         titulo: "Iphone 11",
         costo: 575.0,
-        portada: "https://falabella.scene7.com/is/image/FalabellaPE/18673441_1"
+        portada: "https://falabella.scene7.com/is/image/FalabellaPE/18673441_1",
+        oferta: true
       },
       {
         id: 2,
         titulo: "Samsung TV",
         costo: 675.0,
-        portada: "https://falabella.scene7.com/is/image/FalabellaPE/18891312_01"
+        portada:
+          "https://falabella.scene7.com/is/image/FalabellaPE/18891312_01",
+        oferta: false
       },
       {
         id: 3,
         titulo: "LG TV 55'",
         costo: 845.0,
-        portada: "https://falabella.scene7.com/is/image/FalabellaPE/18946885_01"
+        portada:
+          "https://falabella.scene7.com/is/image/FalabellaPE/18946885_01",
+        oferta: true
       },
       {
         id: 4,
         titulo: "Laptop Lenovo",
         costo: 845.0,
-        portada: "https://falabella.scene7.com/is/image/FalabellaPE/882511912_1"
+        portada:
+          "https://falabella.scene7.com/is/image/FalabellaPE/882511912_1",
+        oferta: true
       },
       {
         id: 5,
         titulo: "Batidora",
         costo: 845.0,
-        portada: "https://falabella.scene7.com/is/image/FalabellaPE/17886849_1"
+        portada: "https://falabella.scene7.com/is/image/FalabellaPE/17886849_1",
+        oferta: false
       },
       {
         id: 6,
         titulo: "Pack Minime",
         costo: 845.0,
-        portada: "https://falabella.scene7.com/is/image/FalabellaPE/18782863"
-      },
-      {
-        id: 7,
-        titulo: "Dispensador",
-        costo: 845.0,
-        portada: "https://falabella.scene7.com/is/image/FalabellaPE/18782863"
-      },
-      {
-        id: 8,
-        titulo: "Wafflera",
-        costo: 845.0,
-        portada: "https://falabella.scene7.com/is/image/FalabellaPE/881985225_1"
+        portada: "https://falabella.scene7.com/is/image/FalabellaPE/18782863",
+        oferta: false
       }
     ]
   },
-  methods: {},
+  methods: {
+    getColor(index) {
+      return index % 2 ? "gris" : "blanco";
+    }
+  },
   computed: {}
 });
