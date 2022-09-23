40d3dd90127e5334ef878f7df26871f2d08c669c
SoleMina <karina.pradogutierrez@gmail.com>
2022-09-23 16:23:43 -0500

    update

--- desafio04/src/components/RegisterForm.vue
+++ desafio04/src/components/RegisterForm.vue
@@ -144,6 +144,8 @@ export default {
       let res = false;
       if (value.includes("@") && value.includes(".com")) {
         res = true;
+        var re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        return re.test(value);
       }
       return res;
     },
